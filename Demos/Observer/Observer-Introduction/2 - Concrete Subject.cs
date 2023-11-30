07f63d507410c425fe792c9706d34129a7ac14e7
Chad Green <chad.green@talelearncode.com>
2023-11-29 23:16:17 -0500

    Documenting the Observer pattern

---
+++ Demos/Observer/Observer-Introduction/2 - Concrete Subject.cs
@@ -0,0 +1,36 @@
+﻿namespace TaleLearnCode.ReevaluatingSoftwareDesignPatterns.Observer;
+
+
+// Concrete Subject that implements the ISubject interface
+public record StockMarket(string Name) : ISubject
+{
+	private double _stockPrice;
+	private readonly List<IObserver> _observers = [];
+
+	public void SetStockPrice(double price)
+	{
+		_stockPrice = price;
+		NotifyObservers();
+	}
+
+	public void RegisterObserver(IObserver observer)
+	{
+		_observers.Add(observer);
+		Console.WriteLine($"Investor {observer.Name} has subscribed for updates to '{Name}'");
+	}
+
+	public void RemoveObserver(IObserver observer)
+	{
+		_observers.Remove(observer);
+		Console.WriteLine($"Investor {observer.Name} has unsubscribed for updates to '{Name}'");
+	}
+
+	public void NotifyObservers()
+	{
+		foreach (var observer in _observers)
+		{
+			observer.Update(_stockPrice);
+		}
+	}
+
+}
